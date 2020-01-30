18f4574ea044b44a1877f4176c5c7bb7c07f4d20
lyj-bupt <liuyajing@bupt.edu.cn>
2020-01-30 11:09:58 +0800

    模拟数据接收

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,6 +9,17 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+//读取json数据，使用express框架
+const express = require('express')
+const app = express()//请求sever
+var appData = require('../data.json')//加载本地数据文件
+var seller = appData.seller//获取对应的本地数据
+var goods = appData.goods
+var ratings = appData.ratings
+//路由处理
+var apiRoutes = express.Router()
+//路由处理
+app.use('/api',apiRoutes)//通过路由请求数据
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -42,6 +53,29 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    //接收GET数据
+    before(app) {
+      app.get('/api/seller', function(req,res){
+        res.json({
+          errno: 0,
+          data: seller
+        })//接口返回json数据，上面配置的数据seller赋值给data请求后调用
+      }),
+      
+      app.get('/api/goods', function(req,res){
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      }),
+      
+      app.get('/api/ratings', function(req,res){
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   },
   plugins: [
